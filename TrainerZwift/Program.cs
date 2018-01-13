6cb08f1909f663431a0c1fc17650ef81d1579a26
m-dwyer <m-dwyer@users.noreply.github.com>
2018-01-14 07:42:40 +1100

    Initial commit

---
+++ TrainerZwift/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrainerZwift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle screenRect = Screen.PrimaryScreen.WorkingArea;
+
+            Process[] processes = Process.GetProcesses();
+            Process zwiftProcess = processes.Where(p => p.MainWindowTitle == "Zwift").FirstOrDefault();
+            Process trainerRoadProcess = processes.Where(p => p.MainWindowTitle == "TrainerRoad").FirstOrDefault();
+
+            // Resize border for existing Zwift instance.  Needs to be launched separately for login
+            if (zwiftProcess != null)
+            {
+                IntPtr zwiftProcessHandle = zwiftProcess.MainWindowHandle;
+
+                Win32.RemoveBorder(zwiftProcessHandle);
+                Win32.MoveWindow(zwiftProcessHandle, 0, 0, screenRect.Width, (int)(screenRect.Height * 0.75));
+            }
+            
+            // We can launch TrainerRoad if it hasn't been already
+            if (trainerRoadProcess == null)
+            {
+                RegistryKey regKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall");
+                string location = FindByDisplayName(regKey, "TrainerRoad for Windows");
+                if (!string.IsNullOrEmpty(location))
+                {
+                    Process.Start($"{location}\\TrainerRoad.Net.exe");
+                }
+            }
+        }
+
+        private static string FindByDisplayName(RegistryKey parentKey, string name)
+        {
+            string[] nameList = parentKey.GetSubKeyNames();
+            for (int i = 0; i < nameList.Length; i++)
+            {
+                RegistryKey regKey = parentKey.OpenSubKey(nameList[i]);
+                try
+                {
+                    if (regKey.GetValue("DisplayName").ToString() == name)
+                    {
+                        return regKey.GetValue("InstallLocation").ToString();
+                    }
+                }
+                catch { }
+            }
+            return "";
+        }
+    }
+}
---
+++ TrainerZwift/Win32.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace TrainerZwift
+{
+    class Win32
+    {
+        const int WS_BORDER = 8388608;
+        const int WS_DLGFRAME = 4194304;
+        const int WS_CAPTION = WS_BORDER | WS_DLGFRAME;
+        const int WS_SYSMENU = 524288;
+        const int WS_THICKFRAME = 262144;
+        const int WS_MAXIMIZE = 16777216;
+        const int WS_MINIMIZE = 536870912;
+        const int WS_MAXIMIZEBOX = 65536;
+        const int GWL_STYLE = -16;
+        const int GWL_EXSTYLE = -20;
+        const int WS_EX_DLGMODALFRAME = 0x1;
+        const int SWP_NOMOVE = 0x2;
+        const int SWP_NOSIZE = 0x1;
+        const int SWP_FRAMECHANGED = 0x20;
+        const uint MF_BYPOSITION = 0x400;
+        const uint MF_REMOVE = 0x1000;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        public static IntPtr GetWindowLong(HandleRef hWnd, int nIndex)
+        {
+            if (IntPtr.Size == 4)
+            {
+                return GetWindowLong32(hWnd, nIndex);
+            }
+            return GetWindowLongPtr64(hWnd, nIndex);
+        }
+
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong", CharSet = CharSet.Auto)]
+        private static extern IntPtr GetWindowLong32(HandleRef hWnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLongPtr", CharSet = CharSet.Auto)]
+        private static extern IntPtr GetWindowLongPtr64(HandleRef hWnd, int nIndex);
+
+        // This static method is required because legacy OSes do not support
+        // SetWindowLongPtr
+        public static IntPtr SetWindowLong(HandleRef hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            if (IntPtr.Size == 4)
+                return new IntPtr(SetWindowLong32(hWnd, nIndex, dwNewLong.ToInt32()));
+            else
+                return SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        private static extern int SetWindowLong32(HandleRef hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+        private static extern IntPtr SetWindowLongPtr64(HandleRef hWnd, int nIndex, IntPtr dwNewLong);
+
+        public static void RemoveBorder(IntPtr handle)
+        {
+            HandleRef handleRef = new HandleRef(null, handle);
+            var blah = (int) GetWindowLong(handleRef, GWL_STYLE);
+            blah &= ~(WS_CAPTION | WS_THICKFRAME | WS_MINIMIZE | WS_MAXIMIZE | WS_SYSMENU);
+            SetWindowLong(new HandleRef(null, handle), GWL_STYLE, new IntPtr(blah));
+        }
+
+        public static void MoveWindow(IntPtr handle, int x, int y, int width, int height)
+        {
+            MoveWindow(handle, x, y, width, height, true);
+        }
+    }
+}
